2f5452c59355439a68ab4cab48272971e6550821
Ganesh Jangiir <jangirg@amazon.com>
2021-04-13 15:10:29 -0700

    Initial commit

---
+++ src/CdkPipelineCrossAccount/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkPipelineCrossAccount
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CdkPipelineCrossAccountStack(app, "CdkPipelineCrossAccountStack", new StackProps
+            {
+            });
+
+            app.Synth();
+        }
+    }
+}
